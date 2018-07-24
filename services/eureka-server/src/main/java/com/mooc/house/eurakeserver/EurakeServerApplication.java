152d5788d3965be44e348e26ba682ce6dc1683ce
guoyukai <yukai.guo@uetec.cn>
2018-07-24 15:19:42 +0800

    初次提交

---
+++ services/eureka-server/src/main/java/com/mooc/house/eurakeserver/EurakeServerApplication.java
@@ -0,0 +1,14 @@
+package com.mooc.house.eurakeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurakeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakeServerApplication.class, args);
+	}
+}
